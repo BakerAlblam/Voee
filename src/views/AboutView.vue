8018f55b579b6d6eb2fa51b9094d7deac4367dc4
BakerAlblam <gallardo0@outlook.com>
2023-07-05 18:58:14 +0200

    vue 2

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,16 +1,16 @@
 <script setup>
-import { useQuery } from "@tanstack/vue-query";
-import axios from "axios";
-import { RouterLink, RouterView, useRoute } from "vue-router";
-import Nav from "../components/Nav.vue";
-import { computed } from "vue";
+import { useQuery } from '@tanstack/vue-query';
+import axios from 'axios';
+import { RouterLink, RouterView, useRoute } from 'vue-router';
+import Nav from '../components/Nav.vue';
+import { computed } from 'vue';
 
 const route = useRoute();
 const id = route.params.id;
 
 const useUserQuery = () => {
   return useQuery({
-    queryKey: ["user"],
+    queryKey: ['user'],
     queryFn: async () => {
       const response = await axios.get(`https://reqres.in/api/users/${id}`);
       const data = await response.data.data;
@@ -23,47 +23,30 @@ const { data } = useUserQuery();
 </script>
 
 <template>
-<Nav />
+  <Nav />
 
-<section class=" bg-[#071e34] flex font-medium items-center justify-center h-screen">
-
-    <section class="w-64 mx-auto bg-[#20354b] rounded-2xl px-14 py-12 shadow-lg">
-      
-        <div class="mt-6 w-fit mx-auto">
-            <img :src="data.avatar" class="rounded-full w-28 " alt="profile picture" srcset="">
-        </div>
-
-        <div class="mt-8 ">
-            <h2 class="text-white font-bold text-2xl tracking-wide text-center">{{ data.first_name }} {{ data.last_name }}</h2>
-        </div>
-
-
-        <hr class="bg-indigo-700 h-1 my-5" />
-
-     
-            
-<div class="flex justify-center mt-5">
-  <a
-    :href="`/about/${parseInt(id) - 1}`"
-    class="px-4 py-2 mx-2 bg-indigo-700 text-white rounded"
+  <section
+    class="bg-[#071e34] flex font-medium items-center justify-center h-screen"
   >
-    Previous
-  </a>
-  <a
-    :href="`/about/${parseInt(id) + 1}`"
-    class="px-4 py-2 mx-2 bg-indigo-700 text-white rounded"
-  >
-    Next
-  </a>
-</div>
-       
+    <section
+      class="w-64 mx-auto bg-[#20354b] rounded-2xl px-14 py-12 shadow-lg"
+    >
+      <div class="mt-6 w-fit mx-auto">
+        <img
+          :src="data.avatar"
+          class="rounded-full w-28"
+          alt="profile picture"
+          srcset=""
+        />
+      </div>
 
+      <div class="mt-8">
+        <h2 class="text-white font-bold text-2xl tracking-wide text-center">
+          {{ data.first_name }} {{ data.last_name }}
+        </h2>
+      </div>
+
+      <hr class="bg-indigo-700 h-1 my-5" />
     </section>
-
-    
-
-</section>
-
+  </section>
 </template>
-
-
